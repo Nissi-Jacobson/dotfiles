38310fcee9e5287c337ab92b4eaaaa9fedbf43ed
Nissi-Jacobson <n.jacobson9652.com>
2025-05-20 00:58:52 +0530

    2nd try of linking nvim to home-manager

--- home.nix
+++ home.nix
@@ -18,7 +18,7 @@
   ];
 
  # adding dotfiles
-  home.file."~/.config/nvim/".source = "./nvim/";
+  home.file.".config/nvim/".source = "./nvim/";
  
   # The home.sessionVariables option allows you to install Nix packages into your
   # sessionVariables
