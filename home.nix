9ddb9e108495c94ad54e194e9d1596a6a5a7b81a
Nissi-Jacobson <n.jacobson9652.com>
2025-05-20 16:40:57 +0530

    adding symlink to nvim

--- home.nix
+++ home.nix
@@ -32,8 +32,10 @@
   ];
 
  # adding dotfiles
-  #home.file.".config/nvim".source = builtins.toPath "./nvim";
- 
+  home.file = {
+    ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "/home/nissi/dotfiles/nvim"
+  }
+
   # The home.sessionVariables option allows you to install Nix packages into your
   # sessionVariables
   home.sessionVariables = {
