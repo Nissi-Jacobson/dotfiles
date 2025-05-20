35df680b694362d7177c5e35b63ebc9120233c7d
Nissi-Jacobson <n.jacobson9652.com>
2025-05-20 22:11:01 +0530

    kitty config continues

--- home.nix
+++ home.nix
@@ -58,13 +58,26 @@
   };
  
   # bash customization
-  programs.bash = {
+  programs.zsh = {
     enable = true;
     shellAliases = {
       ll = "ls -l";
       la = "ls -a";
       ".." = "cd ..";
     };
+    autocd = true;
+    plugins = [
+     # # will source zsh-autosuggestions.plugin.zsh
+     # name = "zsh-autosuggestions";
+     # src = pkgs.fetchFromGitHub {
+     #   owner = "zsh-users";
+     #   repo = "zsh-autosuggestions";
+     #   rev = "v0.4.0";
+     #   sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
+     # };
+    ];
+    enableCompletion = true;
+    autosuggestion.enable = true;
   };
 
   # gnome extension customizations
