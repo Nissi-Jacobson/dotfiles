d527c54b49262c674d731197e5d2dffa8ae4e48d
Nissi-Jacobson <n.jacobson9652.com>
2025-05-22 00:04:48 +0530

    added hibernate button extension

--- user/gnome.nix
+++ user/gnome.nix
@@ -13,6 +13,7 @@
     gnomeExtensions.gsconnect
     gnomeExtensions.hide-top-bar
     gnomeExtensions.status-area-horizontal-spacing
+    gnomeExtensions.hibernate-status-button
     _0xproto
   ];
 
@@ -44,8 +45,8 @@
           cursor-theme = "Adwaita";
           enable-animations = true;
           enable-hot-corners = true;
-          font-name = "0xProto, 12";
-          monospace-font-name = "0xProto 12";
+          font-name = "0xProto, 10";
+          monospace-font-name = "0xProto 10";
           #gtk-theme = "Adwaita";
           scaling-factor = 1;
           text-scaling-factor = 1;
@@ -54,7 +55,7 @@
           allow-volume-above-100-percent = true;
           event-sounds = false;
         };
-        "org/gnome/desktop/vm/preferences" = {
+        "org/gnome/desktop/wm/preferences" = {
           focus-mode = "sloppy";
         };
         "org/gnome/mutter" = {
@@ -70,6 +71,7 @@
           show-directory-item-count = "always";
           show-image-thumbnails = "always";
         };
+        "org/gnome/shell/extensions/blur-my-shell".enable = true;
         "org/gnome/shell/extensions/blur-my-shell/hidetopbar".compatibility = true;
         "org/gnome/shell/extensions/blur-my-shell/panel".static-blur = false;
         "org/gnome/shell/extensions/blur-my-shell/applications" = {
@@ -89,6 +91,9 @@
           mouse-sensitive = true;
           mouse-triggers-overview = false;
           show-in-overview = true;
+        };
+        "org/gnome/shell/extensions/hibernate-status-button" = {
+          show-suspend-then-hibernate-dialog = true;
         };
       };
     };
