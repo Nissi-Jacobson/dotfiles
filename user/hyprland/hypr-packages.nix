8c41f7d7714519e34c4267288d9dc6308d477b7d
Nissi-Jacobson <n.jacobson9652.com>
2025-05-22 23:08:35 +0530

    installing packages with home manager

--- user/hyprland/hypr-packages.nix
+++ user/hyprland/hypr-packages.nix
@@ -4,9 +4,6 @@
   home.packages = with pkgs; [
     wofi
     waybar
-    (waybar.overrideAttrs (oldAttrs: {
-      mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental = true"];
-    }))
     dunst
     libnotify
     hyprpaper
