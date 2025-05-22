7f2a517d5f2d4f3647a974ba694cf6ee916692e5
Nissi-Jacobson <n.jacobson9652.com>
2025-05-22 21:59:19 +0530

    3rd try hypr.nix & downloaded plugins nvim-surround, hardtime

--- user/hyprland/hyprland.nix
+++ user/hyprland/hyprland.nix
@@ -52,7 +52,7 @@
       # Refer to https://wiki.hyprland.org/Configuring/Variables/
       
       # https://wiki.hyprland.org/Configuring/Variables/#general
-      general {
+      general = {
           gaps_in = 0;
           gaps_out = 0;
       
@@ -72,14 +72,14 @@
       };
       
       # https://wiki.hyprland.org/Configuring/Variables/#decoration
-      decoration {
+      decoration = {
           rounding = 10;
       
           # Change transparency of focused and unfocused windows
           active_opacity = 1.0;
           inactive_opacity = 1.0;
       
-          shadow {
+          shadow = {
               enabled = true;
               range = 4;
               render_power = 3;
@@ -87,7 +87,7 @@
           };
       
           # https://wiki.hyprland.org/Configuring/Variables/#blur
-          blur {
+          blur = {
               enabled = true;
               size = 3;
               passes = 1;
@@ -97,7 +97,7 @@
       };
       
       # https://wiki.hyprland.org/Configuring/Variables/#animations
-      animations {
+      animations = {
           enabled = "yes, please :)";
       
           # Default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
@@ -141,18 +141,18 @@
       # windowrulev2 = rounding 0, floating:0, onworkspace:f[1]
       
         # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
-      dwindle {
+      dwindle = {
           pseudotile = true; # Master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
           preserve_split = true; # You probably want this
       };
       
       # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
-      master {
+      master = {
           new_status = master;
       };
       
       # https://wiki.hyprland.org/Configuring/Variables/#misc
-      misc {
+      misc = {
           force_default_wallpaper = -1; # Set to 0 or 1 to disable the anime mascot wallpapers
           disable_hyprland_logo = false; # If true disables the random hyprland logo / anime girl background. :(
       };
@@ -162,7 +162,7 @@
       #############
       
       # https://wiki.hyprland.org/Configuring/Variables/#input
-      input {
+      input = {
         kb_layout = "us";
         kb_variant = "colemak";
         #kb_model =
@@ -173,22 +173,22 @@
     
         sensitivity = 1; # -1.0 - 1.0, 0 means no modification.
     
-        touchpad {
+        touchpad = {
           natural_scroll = false;
         };
       };
     
       # https://wiki.hyprland.org/Configuring/Variables/#gestures
-      gestures {
+      gestures = {
           workspace_swipe = false;
       };
       
       # Example per-device config
       # See https://wiki.hyprland.org/Configuring/Keywords/#per-device-input-configs for more
-      device {
+      device = {
           name = "epic-mouse-v1";
           sensitivity = -0.5;
-      }
+      };
 
 
       ###################
@@ -196,7 +196,7 @@
       ###################
       
       # See https://wiki.hyprland.org/Configuring/Keywords/
-      "$mainMod" = "SUPER" # Sets "Windows" key as main modifier
+      "$mainMod" = "SUPER"; # Sets "Windows" key as main modifier
       
       # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
       bind = [ 
@@ -257,20 +257,20 @@
         
         # Laptop multimedia keys for volume and LCD brightness
       bindel = [
-        ,XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+
-        ,XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
-        ,XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-        ,XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
-        ,XF86MonBrightnessUp, exec, brightnessctl s 10%+
-        ,XF86MonBrightnessDown, exec, brightnessctl s 10%-
+        ",XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+        ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+        ",XF86MonBrightnessUp, exec, brightnessctl s 10%+"
+        ",XF86MonBrightnessDown, exec, brightnessctl s 10%-"
       ];
         
       # Requires playerctl
       bindl = [
-        , XF86AudioNext, exec, playerctl next
-        , XF86AudioPause, exec, playerctl play-pause
-        , XF86AudioPlay, exec, playerctl play-pause
-        , XF86AudioPrev, exec, playerctl previous
+        ", XF86AudioNext, exec, playerctl next"
+        ", XF86AudioPause, exec, playerctl play-pause"
+        ", XF86AudioPlay, exec, playerctl play-pause"
+        ", XF86AudioPrev, exec, playerctl previous"
       ];
     };
   };
