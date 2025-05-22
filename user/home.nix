c944bd29a140449688c0d784f28f998d02a297d0
Nissi-Jacobson <n.jacobson9652.com>
2025-05-22 21:50:27 +0530

    2 try hypr.nix

--- user/home.nix
+++ user/home.nix
@@ -9,7 +9,7 @@
     ./apps/default.nix
     ./qutebrowser.nix
     ./defaultApps.nix
-    ./hyprland.nix
+    ./hyprland/hyprland.nix
   ];
 
   # Home Manager needs a bit of information about you and the paths it should manage.
