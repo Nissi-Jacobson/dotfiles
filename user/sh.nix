e5c8d87840b9c10714df703e927f208f6def83a2
Nissi-Jacobson <n.jacobson9652.com>
2025-05-22 10:52:24 +0530

    made all the keywords fucking strings in sh.nix

--- user/sh.nix
+++ user/sh.nix
@@ -2,13 +2,13 @@
 
 let
   myShellAliases = {
-    ll = "ls -l";
-    la = "ls -a";
-    lal = "ls -al";
+    "ll" = "ls -l";
+    "la" = "ls -a";
+    "lal" = "ls -al";
     ".." = "cd ..";
-    dotnvim = "cd ~/dotfiles/ && nvim";
-    obsnvim = "cd ~/Documents/brain-dump/ && nvim";
-    codenvim = "cd ~/Documents/codes && nvim";
+    "dotnvim" = "cd ~/dotfiles/ && nvim";
+    "obsnvim" = "cd ~/Documents/brain-dump/ && nvim";
+    "codenvim" = "cd ~/Documents/codes && nvim";
   };
 in
 
