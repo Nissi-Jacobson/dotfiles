1eb9fc17a483c7d01e0abeefa1b3b09758e7782d
Nissi-Jacobson <n.jacobson9652.com>
2025-05-22 14:26:53 +0530

    uninstalling every downloaded package

--- system/packages.nix
+++ system/packages.nix
@@ -3,17 +3,17 @@
 {
   environment.systemPackages = with pkgs; [
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    git
-    neovim
-    clang
-    firefox
-    zsh
-    fish
-    ghostty
-    direnv
-    nodePackages.nodejs
-    wl-clipboard
+    #wget
+    #git
+    #neovim
+    #clang
+    #firefox
+    #zsh
+    #fish
+    #ghostty
+    #direnv
+    #nodePackages.nodejs
+    #wl-clipboard
   ];
 }
 
