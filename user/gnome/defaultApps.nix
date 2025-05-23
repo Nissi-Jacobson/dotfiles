c11d8c836eda6cf194c57fae606cbecda7a5338a
Nissi-Jacobson <n.jacobson9652.com>
2025-05-23 19:13:48 +0530

    23May NixOS + gnome + hyprland

--- user/gnome/defaultApps.nix
+++ user/gnome/defaultApps.nix
@@ -10,7 +10,6 @@
         "x-scheme-handler/http" = "brave-browser.desktop";
         "x-scheme-handler/https" = "brave-browser.desktop";
         "x-scheme-handler/about" = "brave-browser.desktop";
-        "x-scheme-handler/unknown" = "brave-browser.desktop";
         "image" = [
         "feh.desktop"
         "qimgv.desktop"
